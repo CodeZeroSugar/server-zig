ab32b42e5dcbee0fc89eb308a5d3c1a542dea528
Ian Velder <icvelder@gmail.com>
2026-05-07 23:24:16 -0500

    basic server, reads one request and displays to console

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
-const server = @import("server.zig");
+const Server = @import("server.zig").Server;
+const Request = @import("request.zig");
 const Io = std.Io;
 
 pub fn main(init: std.process.Init) !void {
@@ -11,4 +12,15 @@ pub fn main(init: std.process.Init) !void {
     const stdout_writer = &stdout_file_writer.interface;
 
     try stdout_writer.flush(); // Don't forget to flush!
+
+    const server = try Server.init(io);
+    var listener = try server.listen();
+    const connection = try listener.accept(io);
+    defer connection.close(io);
+
+    var request_buffer: [1000]u8 = undefined;
+    @memset(request_buffer[0..], 0);
+    try Request.read_request(io, connection, request_buffer[0..]);
+
+    std.debug.print("{s}", .{request_buffer});
 }
